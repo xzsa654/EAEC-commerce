9491790c814c8597f73d38d43014c21af86b3837
Billy <xzsa654@gmail.com>
2025-04-22 19:24:16 +0800

    調整 axios mode

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios"
 
 const axiosInstance = axios.create(
   {
-    baseURL:import.meta.env.MODE==="development"?"http://localhost:3000/api":"",
-    withCredentials:true // 送出 cookies
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+    withCredentials: true // 送出 cookies
   }
 )
 
